2f5370975d86a55be5209db02e6752ea70a91d67
Aradhay-tech <kopulwararadhay@gmail.com>
2025-08-14 13:08:56 +0530

    Initial commit

---
+++ Task-2.py
@@ -0,0 +1,10 @@
+import math
+
+n = int(input("enter a No: "))
+sqrt_value = math.sqrt(n)
+log_value = math.log(n)
+sine_value = math.sin(n)
+
+print("Square root: ",sqrt_value)
+print("Logarithm: ",log_value)
+print("sine: ",sine_value)
